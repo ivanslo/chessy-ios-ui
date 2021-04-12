4c47040d0413f03423db0d2cbbd973659c7d2f12
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2021-04-12 09:08:25 +0100

    Initial Commit

---
+++ chessy-ios-uiTests/chessy_ios_uiTests.swift
@@ -0,0 +1,34 @@
+//
+//  chessy_ios_uiTests.swift
+//  chessy-ios-uiTests
+//
+//  Created by Slobodiuk, Ivan on 12/04/2021.
+//  Copyright © 2021 Slobodiuk, Ivan. All rights reserved.
+//
+
+import XCTest
+@testable import chessy_ios_ui
+
+class chessy_ios_uiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
