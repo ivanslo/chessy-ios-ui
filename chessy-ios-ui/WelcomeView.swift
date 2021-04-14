d08c05920a06f4cc18e9256b4161e7fc299c69ac
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2021-04-14 10:12:09 +0100

    first change: ContentView -> WelcomeView

---
+++ chessy-ios-ui/WelcomeView.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeView.swift
+//  chessy-ios-ui
+//
+//  Created by Slobodiuk, Ivan on 12/04/2021.
+//  Copyright © 2021 Slobodiuk, Ivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Chessy iOS App")
+                .bold()
+            Spacer()
+            MyButton(text: "Search Games")
+            MyButton(text:"Login")
+            MyButton(text:"About")
+            Spacer()
+        }
+    }
+}
+
+struct MyButton: View {
+    var text: String
+    var body: some View {
+        Button(action:{}){
+            Text(text).border(Color.red, width:3)
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
