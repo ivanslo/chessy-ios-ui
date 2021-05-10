50109caecb2939a8ccabd11433883e9d36571381
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2021-05-10 09:25:36 +0100

    Add ChessBoard shape

--- chessy-ios-ui/Views/Board.swift
+++ chessy-ios-ui/Views/Board.swift
@@ -8,11 +8,37 @@
 
 import SwiftUI
 
+struct ChessBoard: Shape {
+    /*
+     ChessBoard Shape draws the squares of the 'dark' pieces.
+     To theme the board one should `fill` with one color and `background` with another.
+     */
+    func path( in rect:CGRect) -> Path {
+        var path = Path()
+
+        let w = rect.width / 8
+        let h = rect.height / 8
+
+        for i in 0..<8 {
+            for j in 0..<8 {
+                if (i+j).isMultiple(of: 2){
+                    let startX = CGFloat(i) * w
+                    let startY = CGFloat(j) * h
+                    let rect = CGRect(x:startX, y:startY, width: w, height: h)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
 struct Board: View {
     var body: some View {
-        Text("")
-        .frame(width:300, height:300)
-        .background(Color.gray)
+        ChessBoard()
+            .fill(Color.red)
+            .background(Color.orange)
+            .frame(width:300, height:300)
     }
 }
 
