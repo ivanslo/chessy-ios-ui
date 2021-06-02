5efa4cf7417f2a6c4b044617aed92568ebafac75
Ivan Slobodiuk <slobodiu@amazon.com>
2021-06-02 10:25:48 +0100

    Style prev-next buttons

--- chessy-ios-ui/Views/GameInfoDetail.swift
+++ chessy-ios-ui/Views/GameInfoDetail.swift
@@ -17,43 +17,47 @@ extension Comparable {
     }
 }
 
+struct GradientButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .leading, endPoint: .trailing).rotationEffect(Angle.init(degrees: 90)))
+            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .font(.largeTitle)
+            .foregroundColor(.white)
+    }
+}
 
 struct GameInfoDetail: View {
     var gameDetail: GameDetail
 
-    func playNext(_ nr : Int) -> Int {
-        return (nr + 1).clamped(0, gameDetail.jsonFileParsed.steps.count-1)
+    func playNext() -> Void {
+        self.idx = (self.idx + 1).clamped(0, gameDetail.jsonFileParsed.steps.count-1)
     }
-    func playBefore(_ nr : Int) -> Int {
-        return (nr - 1).clamped(0, gameDetail.jsonFileParsed.steps.count-1)
+    func playBefore () -> Void {
+        self.idx = (self.idx - 1).clamped(0, gameDetail.jsonFileParsed.steps.count-1)
     }
 
     @State private var idx: Int = 0
     var body: some View {
-        
         VStack{
             ZStack{
                 Board()
                 Pieces(arrangementStr: gameDetail.jsonFileParsed.steps[idx].board)
-            }.frame(width: 370, height: 370)
+            }.aspectRatio(1, contentMode: .fit)
 
             HStack{
-                Button(action:{
-                    self.idx = self.playBefore(self.idx)
-                }){
-                    Text("<")
-                }
-                .frame(width: 70, height: 50)
-                .background(Color.gray)
-                Button(action:{
-                    self.idx = self.playNext(self.idx)
-                }){
-                    Text(">")
-                }
-                .frame(width: 70, height: 50)
-                .background(Color.gray)
+                Button(action: playBefore , label: {
+                    Image(systemName: "chevron.left.circle")
+                })
+                    .buttonStyle(GradientButtonStyle())
+                Button(action: playNext , label: {
+                    Image(systemName: "chevron.right.circle")
+                })
+                    .buttonStyle(GradientButtonStyle())
             }
             Text(gameDetail.jsonFileParsed.steps[idx].board)
+                .font(.caption)
         }
 
     }
